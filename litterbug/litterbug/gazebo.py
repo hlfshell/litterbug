f29adc3588a8263af053f4f3b045a83c7bce9fa0
keith <kchester@gmail.com>
2024-01-10 19:33:19 -0800

    fix: use config

--- litterbug/litterbug/gazebo.py
+++ litterbug/litterbug/gazebo.py
@@ -1,4 +1,5 @@
 from gazebo_msgs.srv import DeleteEntity, GetEntityState, GetModelList, SpawnEntity
+from litterbug.config import LitterbugConfig
 
 
 class Gazebo(Node):
@@ -10,22 +11,25 @@ class Gazebo(Node):
 
     def __init__(
         self,
-        service_name: str,
-        models_dir: str,
+        config: LitterbugConfig,
     ):
+        service_name = config["gazebo.service_name"]
         super().__init__(service_name)
-        self.__models_dir = models_dir
-        self.__service_name = service_name
+        self.__models_directories = config["gazebo.models_directories"]
 
-        self.__spawn_entity_client = self.create_client(SpawnEntity, "/spawn_entity")
-        self.__delete_entity_client = self.create_client(DeleteEntity, "/delete_entity")
+        self.__spawn_entity_client = self.create_client(
+            SpawnEntity, config["gazebo.topics.spawn_entity"]
+        )
+        self.__delete_entity_client = self.create_client(
+            DeleteEntity, config["gazebo.topics.delete_entity"]
+        )
         self.__get_model_list_client = self.create_client(
-            GetModelList, "/get_model_list"
+            GetModelList, config["gazebo.topics.get_model_list"]
         )
         self.__get_entity_state_client = self.create_client(
-            GetEntityState, "/gazebo/get_entity_state"
+            GetEntityState, config["gazebo.topics.get_entity_state"]
         )
         # Test
         self.__get_entity_exists_client = self.create_client(
-            GetEntityState, "/gazebo/get_entity_state"
+            GetEntityState, config["gazebo.topics.get_entity_state"]
         )
